5be7f2159456ebe4608610a10f8666ab80e9566a
Julia Veronica <julia.veronicagdu@gmail.com>
2020-05-23 01:08:36 +0700

    done

--- controllers/User.js
+++ controllers/User.js
@@ -111,4 +111,45 @@ module.exports = {
         throw err;
       });
   },
+  login: ((req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    // Find user by email
+    User
+        .findOne({ where: { email: req.body.email } })
+        .then(user => {
+            // Check if user exists
+            if (!user) {
+                return res
+                    .status(404)
+                    .json({emailnotfound: "Email not found"});
+            }
+            // Check password
+            bcrypt
+                .compare(password, user.password)
+                .then(isMatch => {
+                    if (isMatch) {
+                        // User matched Create JWT Payload
+                        const payload = {
+                            id: user.id,
+                            name: user.name
+                        };
+                        // Sign token
+                        jwt.sign(payload, privateKey, {
+                            expiresIn: 31556926 // 1 year in seconds
+                        }, (err, token) => {
+                            res.json({
+                                success: true,
+                                token: "Bearer " + token,
+                                id: user.id
+                            });
+                        });
+                    } else {
+                        return res
+                            .status(400)
+                            .json({passwordincorrect: "Password incorrect"})
+                    }
+                })
+        })
+})
 };
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const User = require("../controllers/User");
 /* GET users listing. */
 router.post("/register", User.create);
-router.post("/login", User.authenticated);
+router.post("/login", User.login);
 router.get("/show", User.getData);
 router.get("/show/:id", User.getDataById);
 module.exports = router;
