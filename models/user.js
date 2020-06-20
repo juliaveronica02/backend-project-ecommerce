1c0024e8e86dd5cb4bbb8c37e2d7341e9df161ba
juliaveronica02 <juliavero2206@gmail.com>
2020-06-20 13:45:04 +0700

    models user add role

--- models/user.js
+++ models/user.js
@@ -3,7 +3,8 @@ module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     email: DataTypes.STRING,
     phone: DataTypes.INTEGER,
-    password: DataTypes.STRING
+    password: DataTypes.STRING,
+    role: DataTypes.STRING
   }, {});
   User.associate = function(models) {
     // associations can be defined here
